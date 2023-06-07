8afda5d5ae575913db6a1d8fd1098371ad9740b8
ZRA <antdaddario@gmail.com>
2023-06-07 15:16:03 -0500

    changed environment variables

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 const MongoStore = require('connect-mongo');
-const dbUrl = 'mongodb://localhost:27017/yelp-camp'
-// const dbUrl = process.env.DB_URL
+// const dbUrl = 'mongodb://localhost:27017/yelp-camp'
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp"
 // mongodb://localhost:27017/yelp-camp
 mongoose.set('strictQuery', true);
 mongoose.connect(dbUrl, {
@@ -48,12 +48,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(mongoSanitize({
     replaceWith: '_'
 }))
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!'
 
 const store = MongoStore.create({
     mongoUrl: dbUrl,
     touchAfter: 24 * 60 * 60,
     crypto: {
-        secret: 'thisshouldbeabettersecret!'
+        secret
     }
 });
 
@@ -64,7 +65,7 @@ store.on("error", function (e) {
 const sessionConfig = {
     store,
     name: 'session',
-    secret: 'thisshouldbeabettersecret!',
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
